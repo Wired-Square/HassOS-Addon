a9f737c4cebac158e24bc20ab849f355746cf39b
Alex Ferrara <alex@wiredsquare.com>
2023-02-17 15:57:20 +1100

    0.0.8

--- WiredCAN/rootfs/run.sh
+++ WiredCAN/rootfs/run.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/with-contenv bash
-whoami
-id
+#whoami
+#id
 
-echo $0
+#echo $0
 
-nc -lk -p 8099 -e  echo -e 'HTTP/1.1 200 OK\r\nServer: DeskPiPro\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>Wired Square. Seriously!</p></body></html>\r\n\n\n' & 
+#nc -lk -p 8099 -e  echo -e 'HTTP/1.1 200 OK\r\nServer: DeskPiPro\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>Wired Square. Seriously!</p></body></html>\r\n\n\n' & 
 
 until false; do
   sleep 14400
