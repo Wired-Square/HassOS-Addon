52ad9263e050adcd2c8da272212c290482bca806
Alex Ferrara <alex@wiredsquare.com>
2023-02-18 21:54:02 +1100

    0.0.27

--- WiredCAN/rootfs/run.sh
+++ WiredCAN/rootfs/run.sh
@@ -2,6 +2,10 @@
 
 /usr/sbin/can-util configure
 
+mkdir -p /config/custom_components
+
+git clone git@github.com:garthberry/homeassistant-canswitch.git /config/custom_components/canswitch
+
 # Keep the docker container alive. This will eventually not be necessary.
 #
 
